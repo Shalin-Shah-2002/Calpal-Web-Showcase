a29a9a1c9c586cf43a5c1f26002323f6585b2cab
Shalin-Shah-2002 <2002shalin@gmail.com>
2026-03-27 13:51:57 +0530

    feat: Add Privacy Policy page and update navigation links

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PrivacyPolicy from './PrivacyPolicy.jsx'
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '').toLowerCase()
+const isPrivacyPolicyPage = normalizedPath === '/privacy-policy'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isPrivacyPolicyPage ? <PrivacyPolicy /> : <App />}
   </StrictMode>,
 )
